b23c1bcac2792921ec518462049588e83ef27285
Dylan R <d.raiff@icloud.com>
2021-11-02 07:51:45 -0400

    init update

--- towr.py
+++ towr.py
@@ -2,27 +2,207 @@ import sys
 import os
 from enum import Enum, auto
 from dataclasses import dataclass
+from typing import Union
+
+class Keyword(Enum):
+    LET         = auto()
+    END         = auto()
+    # IF          = auto()
+    # FUNC        = auto()
+
+class Intrinsic(Enum):
+    pass
 
 class OperationType(Enum):
     PUSH_INT    = auto()
     PLUS        = auto()
     MULTIPLY    = auto()
+    PRINT       = auto()
+    SWAP        = auto()
+    WRITE_MEM   = auto()
+    RET_MEM     = auto()
 
-class Operation:
-    pass
+    NO_OP       = auto()
 
 @dataclass
-class Program:
-    operations: list[Operation]
+class Operation:
+    type: OperationType
+    operand: int
 
-def usage():
+@dataclass
+class Func:
+    func_body: str
+
+KEYWORDS: dict[str, Keyword] = {
+    'let': Keyword.LET,
+    'end': Keyword.END,
+}
+assert len(KEYWORDS) == len(Keyword), 'Unassigned keywords'
+
+INTRINSICS: dict[str, Intrinsic] = {
+
+}
+assert len(INTRINSICS) == len(Intrinsic), 'Unassigned intrinsics'
+
+OPERATION_CONVERTER = {
+    'funcs': {
+        'isdigit'   : OperationType.PUSH_INT,
+    },
+    'direct': {
+        '+'         : OperationType.PLUS,
+        '*'         : OperationType.MULTIPLY,
+        '^'         : OperationType.PRINT,
+        '~'         : OperationType.SWAP,
+        '@m'        : OperationType.WRITE_MEM, 
+        '!m'        : OperationType.RET_MEM,
+    },
+}
+
+def in_converter(token: str) -> Union[Operation, None]:
+    if token in OPERATION_CONVERTER['direct']:
+        return Operation(
+            type=OPERATION_CONVERTER['direct'][token],
+            operand=0
+        )
+
+    for condition in OPERATION_CONVERTER['funcs']:
+        condition_func = getattr(token, condition)
+        if condition_func():
+            return Operation(
+            type=OPERATION_CONVERTER['funcs'][condition],
+            operand=int(token)
+        )
+
+    return None
+
+def load_variable_to_stack(stack: list[int], vars: dict[str, int], memory: list[bytearray], var_name: str) -> None:
+    mem_idx = memory[vars[var_name]]
+    print(mem_idx)
+
+def parse_token(stack: list[int], vars: dict[str, int], memory: list[bytearray], tokens: list[str], token: str, i: int) -> int:
+    ret_val: int = 1
+    rtokens = tokens[i + 1:]
+
+    if token in KEYWORDS:
+        assert len(Keyword) == 2, 'Unimplemented keywords'
+        keyword = KEYWORDS[token]
+        if keyword == Keyword.LET:
+            if len(rtokens) == 0:
+                assert False, 'EOF unexpected'
+            if 'end' not in rtokens:
+                assert False, '`let` statement never closed'
+           
+            var_name = tokens[i + 1]
+            if var_name in KEYWORDS or var_name in INTRINSICS:
+                assert False, 'trying to overwrite keyword/intrinsic in `let` statement'
+            if var_name in vars:
+                assert False, 'trying to initialize an existing variable'
+
+            ltokens = rtokens[i + 1:rtokens.index('end')]
+            lstack: list[int] = []
+
+            for ltoken in ltokens:
+                if ltoken in vars:
+                    var_contents = vars[ltoken]
+                    lstack.append(var_contents)
+
+                elif (operation := in_converter(ltoken)):
+                    if operation.type == OperationType.PUSH_INT:
+                        lstack.append(operation.operand)
+                    elif operation.type == OperationType.PLUS:
+                        a = lstack.pop()
+                        b = lstack.pop()
+                        lstack.append(a + b)
+                    elif operation.type == OperationType.MULTIPLY:
+                        a = lstack.pop()
+                        b = lstack.pop()
+                        lstack.append(a * b)
+                    else:
+                        assert False, f'unsupported operation {ltoken!r} in `let` statement'
+
+                else:
+                    assert False, f'unknown token {ltoken!r} in `let` statement'
+
+            vars[var_name] = len(memory)
+            lval = lstack.pop()
+            memory.append(bytearray(lval.to_bytes(8, 'big')))
+            ret_val += len(ltokens) + 2
+
+            #assert False, 'LET unimplemented'
+        elif keyword == Keyword.END:
+            assert False, 'END unimplemented'
+
+    elif token in INTRINSICS:
+        assert False, 'TODO: intrinsics not implemented'
+
+    elif (operation := in_converter(token)):
+        # NO_OP does not need implementation
+        assert len(OperationType) - 1 == 7, 'Unimplemented operation types'
+        if operation.type == OperationType.PUSH_INT:
+            stack.append(operation.operand)
+        elif operation.type == OperationType.PLUS:
+            a = stack.pop()
+            b = stack.pop()
+            stack.append(a + b)
+        elif operation.type == OperationType.MULTIPLY:
+            a = stack.pop()
+            b = stack.pop()
+            stack.append(a * b)
+        elif operation.type == OperationType.PRINT:
+            a = stack.pop()
+            print(a)
+        elif operation.type == OperationType.SWAP:
+            a = stack.pop()
+            b = stack.pop()
+            stack.append(b)
+            stack.append(a)
+        elif operation.type == OperationType.WRITE_MEM:
+            assert False, 'TODO: not implemented'
+        elif operation.type == OperationType.RET_MEM:
+            assert False, 'TODO: not implemented'
+        else:
+            assert False, 'impossible, something wrong in `in_converter`'
+
+    elif token in vars:
+        load_variable_to_stack(stack, vars, memory, token)
+
+    else:
+        assert False, f'Token {token!r} not recognized'
+
+    return ret_val
+
+def simulate(code: str) -> None:
+    stack: list[int] = []
+    ptrs: dict[str, int] = {}
+    memory: list[bytearray] = []
+    tokens: list[str] = []
+
+    curr_token: str = ''
+    for c in code:
+        if c == ' ' or c == '\n':
+            if curr_token:
+                tokens.append(curr_token)
+                curr_token = ''
+            continue
+        curr_token += c
+    if curr_token:
+        tokens.append(curr_token)
+
+    token_pointer = 0
+    while token_pointer < len(tokens):
+        token = tokens[token_pointer]
+        token_pointer += parse_token(stack, ptrs, memory, tokens, token, token_pointer)
+    
+
+
+def usage() -> None:
     """ Prints usage and exits with an exit code of 1. """
     print('python towr.py <SUBCOMMAND>')
     print('     sim     Simulates the program in Python')
     print('     com     Compiles the program')
     exit(1)
 
-def main():
+def main() -> None:
     if len(sys.argv) < 2:
         print('ERROR: Missing subcommand')
         usage()
@@ -32,7 +212,21 @@ def main():
         usage()
 
     filepath = os.path.join(os.path.dirname(__file__), sys.argv[2])
-    print(filepath)
+    towr_code: str
+    with open(filepath, 'r') as file:
+        towr_code = file.read()
+
+    subcommand = sys.argv[1]
+    if subcommand == 'sim':
+        simulate(towr_code)
+        # assert False, 'TODO: not implemented'
+    elif subcommand == 'com':
+        assert False, 'TODO: not implemented'
+    else:
+        print(f'ERROR: Subcommand {subcommand} not recognized')
+        usage()
+
+    
 
 if __name__ == '__main__':
     main()
