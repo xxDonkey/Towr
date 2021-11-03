f3a347bc32e3e65314b1e96bdb48c7541e70064b
Dylan R <d.raiff@icloud.com>
2021-11-03 15:02:19 -0400

    Update towr.py

--- towr.py
+++ towr.py
@@ -17,22 +17,30 @@ class Intrinsic(Enum):
     SWAP        = auto()
     WRITE_MEM   = auto()
     RET_MEM     = auto()
+    EQUALS      = auto()
+    GREATER     = auto()
+    LESS        = auto()
 
 class OperationType(Enum):
     PUSH_INT    = auto()
+    PUSH_BOOL   = auto()
+    PUSH_STR    = auto()
     KEYWORD     = auto()
     INTRINSIC   = auto()
 
 class DataType(Enum):
     INT         = auto()
     BOOL        = auto()
-    STRING      = auto()
+    STR         = auto()
 
+# (filename, line, col)
 Location = Tuple[str, int, int]
 
 @dataclass
 class Token:
-    type: Union[Keyword, Intrinsic]
+    type: OperationType
+    keyword: Union[Keyword, None]
+    intrinsic: Union[Intrinsic, None]
     operand: int
     location: Location
     
@@ -51,6 +59,11 @@ class StackValue:
     datatype: DataType
     value: Union[int, bool, str]
 
+@dataclass
+class TowrFile:
+    code_body: str
+    filename: str
+
 KEYWORDS: dict[str, Keyword] = {
     'let'   : Keyword.LET,
     'end'   : Keyword.END,
@@ -64,64 +77,110 @@ INTRINSICS: dict[str, Intrinsic] = {
     '~'     : Intrinsic.SWAP,
     '!m'    : Intrinsic.WRITE_MEM, 
     '@m'    : Intrinsic.RET_MEM,
+    '='     : Intrinsic.EQUALS,
+    '>'     : Intrinsic.GREATER,
+    '<'     : Intrinsic.LESS,
 }
 assert len(INTRINSICS) == len(Intrinsic), 'Unassigned intrinsics'
 
 DATATYPES: dict[type, DataType] = {
     int     : DataType.INT,
     bool    : DataType.BOOL,
-    str     : DataType.STRING,
+    str     : DataType.STR,
 }
 assert len(DATATYPES) == len(DataType), 'Unassigned datatypes'
 
-def load_variable_to_stack(stack: list[int], vars: dict[str, int], memory: list[bytearray], var_name: str, is_ref: bool=False) -> None:
-    val: int
-    if not is_ref:
-        val_bytearr = memory[vars[var_name]]
-        val = int.from_bytes(bytes(val_bytearr), 'big')
-    else:
-        val = vars[var_name]
-    stack.append(val)
-
-def parse_token(stack: list[int], vars: dict[str, int], memory: list[bytearray], tokens: list[str], token: str, i: int) -> int:
-    ret_val: int = 1
-    
-
-    return ret_val
-
-def simulate(code: str) -> None:
-    program: Program = Program()
+def sim_program(program: Program) -> None:
+    op: int = program.operation_pointer
+    ops: list[Token, Func] = program.operations
     stack: list[StackValue] = []
 
+    while op < len(ops):
+        operation = ops[op]
 
+        if isinstance(operation, Token):
+            if operation.type == OperationType.KEYWORD:
+                assert False, '`Keywords` not implemented'
+
+            elif operation.type == OperationType.INTRINSIC:
+                if operation.intrinsic == Intrinsic.PLUS:
+                    pass
+                assert False, '`Intrinsics` not implemented'
+
+            else:
+                assert len(OperationType) == 5, 'Unhandled elements of `OperationType`'
+                if operation.type == OperationType.PUSH_INT:
+                    pass
+                elif operation.type == OperationType.PUSH_BOOL:
+                    pass
+                elif operation.type == OperationType.PUSH_STR:
+                    pass
+
+        elif isinstance(operation, Func):
+            assert False, '`Funcs` not implemented'
+
+        op += 1
     
-    stack: list[int] = []
-    ptrs: dict[str, int] = {}
-    memory: list[bytearray] = []
 
-    tokens: list[str] = []
+def token_from_string(token: str, line: int, col: int, filename: str):
+    operation_type: Union[OperationType, None] = None
+    operand: int = 0
+    keyword: Union[Keyword, None] = None
+    intrinsic: Union[Intrinsic, None] = None
 
-    line, col = 0, 0
+    if token in KEYWORDS:
+        operation_type = OperationType.KEYWORD
+        keyword = KEYWORDS[token]
+
+    elif token in INTRINSICS:
+        operation_type = OperationType.INTRINSIC
+        intrinsic = INTRINSICS[token]
+    
+    elif token.isdigit():
+        operation_type = OperationType.PUSH_INT
+        operand = int(token)
+
+    else:
+        assert False, f'Unknown token {token!r}'
+
+    return Token(
+        type=operation_type,
+        keyword=keyword,
+        intrinsic=intrinsic,
+        operand=operand,
+        location=(filename, line, col)
+    )
+
+def parse_to_program(file: TowrFile) -> Program:
+    code_body: str = file.code_body
+    filename: str = file.filename
+    tokens: list[Token] = []
+
+    line, col = 0, -1
     curr_token: str = ''
-    for c in code:
+    for c in code_body:
+        col += 1
         if c == ' ' or (is_newline := c == '\n'):
             if is_newline:
                 line += 1
                 col = 0
             
             if curr_token:
-                tokens.append(curr_token)
+                token = token_from_string(curr_token, line, col, filename)
+                tokens.append(token)
                 curr_token = ''
+
             continue
         curr_token += c
-        col += 1
     if curr_token:
-        tokens.append(curr_token)
+        token = token_from_string(curr_token, line, col, filename)
+        tokens.append(token)
 
-    token_pointer = 0
-    while token_pointer < len(tokens):
-        token = tokens[token_pointer]
-        token_pointer += parse_token(stack, ptrs, memory, tokens, token, token_pointer)
+    # TODO: function interpretation, let interpretation
+
+    program: Program = Program(operations=tokens)
+    return program
+
     
 
 
@@ -141,15 +200,16 @@ def main() -> None:
         print('ERROR: No file input')
         usage()
 
+    filename = sys.argv[2]
     filepath = os.path.join(os.path.dirname(__file__), sys.argv[2])
-    towr_code: str
+    code_body: str
     with open(filepath, 'r') as file:
-        towr_code = file.read()
+        code_body = file.read()
 
     subcommand = sys.argv[1]
     if subcommand == 'sim':
-        simulate(towr_code)
-        # assert False, 'TODO: not implemented'
+        program = parse_to_program(TowrFile(code_body, filename))
+        sim_program(program)
     elif subcommand == 'com':
         assert False, 'TODO: not implemented'
     else:
