2eca0b25d115892e6e9393d042c4bd8223f38c45
Dylan R <d.raiff@icloud.com>
2021-11-06 08:28:26 -0400

    started `com_program_win10`

--- src/com.py
+++ src/com.py
@@ -1,9 +1,24 @@
 import os
 from globals import *
 
-def com_program_win10(program: Program, outfile: str):
-    pass
 
-filename = 'hello.asm'
-filepath = os.path.join(os.getcwd(), filename)
-os.system(f'\\masm32\\bin\\ml /c /Zd /coff {filepath}')
+
+def com_program_win10(program: Program, outfile: str):
+    code_body: str = ''
+    indentation: int = 0
+
+    def indent():
+        return ' ' * 4 * indentation
+
+    for operation in program.operations:
+        if operation.type == OperationType.OPEN_IF:
+            code_body += indent() + '.if '
+        elif operation.type == OperationType.CLOSE_IF:
+            pass
+        elif operation.type == OperationType.CHECK_STACK_SIZE:
+            pass
+
+
+# filename = 'hello.asm'
+# filepath = os.path.join(os.getcwd(), filename)
+# os.system(f'\\masm32\\bin\\ml /c /Zd /coff {filepath}')
